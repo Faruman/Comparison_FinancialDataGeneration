eb097bc7f887182c6671c5203824738704db3549
unknown <faruman.der.weise@googlemail.com>
2024-07-16 15:55:02 +0800

    updated clustering

--- parameterSearch/IbmSynth/paramSearch_WGANGP.py
+++ parameterSearch/IbmSynth/paramSearch_WGANGP.py
@@ -42,9 +42,7 @@ add_transaction_clusters = True
 ## replace source_id and target_id with graph structure of ids
 if not os.path.exists("./working/transformed_pca_extd_df_graph.csv"):
     real_data = pd.read_csv("../../data/IbmSynth/transformed_df.csv", index_col=0)
-    real_data = real_data.reset_index()
-    real_data["index"] = pd.to_numeric(real_data["index"]).astype(int)
-    real_data = real_data.rename(columns={"index": "timeIndicator"})
+    real_data = real_data.rename(columns={"datetimeIndicator": "timeIndicator"})
 
     if add_transaction_clusters:
         cluster_data = real_data
