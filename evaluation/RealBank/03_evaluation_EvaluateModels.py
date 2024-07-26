5b3edf164345e4e7e842bfa88842bc3a19dca2bf
unknown <faruman.der.weise@googlemail.com>
2024-07-26 19:06:18 +0800

    added evaluation and graph evaluation to real bank data

---
+++ evaluation/RealBank/03_evaluation_EvaluateModels.py
@@ -0,0 +1,86 @@
+import os.path
+import pickle
+import numpy as np
+import pandas as pd
+
+from sdv.metadata import SingleTableMetadata
+from modified_sitepackages.sdv.evaluation.single_table import evaluate_quality
+from sdmetrics.single_table import NewRowSynthesis
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from scipy.spatial.distance import cdist
+
+
+models = ['DOPPELGANGER', 'FINDIFF', 'TVAE', 'WGAN', 'CTGAN']
+keep_col = ['PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10']
+
+real_data = pd.read_csv("./working/transformed_df_graph.csv")
+real_data = real_data.sample(200000)
+
+if not os.path.exists("./results"):
+    os.makedirs("./results")
+
+if os.path.exists("./results/evaluation.xlsx"):
+    results_df = pd.read_excel("./results/evaluation.xlsx")
+else:
+    results_df = pd.DataFrame()
+
+for model in models:
+    print("Currently Processing Model: {}".format(model))
+    with open("./model/{}.pkl".format(model), 'rb') as file:
+        synthesizer = pickle.load(file)
+
+    synthetic_data = synthesizer.sample(num_rows= real_data.shape[0])
+
+    metadata = SingleTableMetadata()
+    metadata.detect_from_dataframe(real_data[keep_col])
+
+    # Evaluate Fidelity
+    quality_report = evaluate_quality(real_data=real_data[keep_col], synthetic_data=synthetic_data[keep_col], metadata=metadata)
+    fidelity_dict = {**quality_report.get_properties().set_index("Property")["Score"].to_dict()}
+
+    # Evaluate Synthesis
+    synthesis_dict = NewRowSynthesis.compute_breakdown(
+        real_data= real_data[keep_col],
+        synthetic_data= synthetic_data[keep_col],
+        metadata= metadata,
+        numerical_match_tolerance=0.01
+    )
+
+    # Evaluate Privacy
+    real_data_oh = pd.DataFrame()
+    synthetic_data_oh = pd.DataFrame()
+    for column_name, sdtype in metadata.columns.items():
+        if sdtype["sdtype"] == 'categorical':
+            ohe = OneHotEncoder()
+            temp = pd.DataFrame(ohe.fit_transform(real_data[column_name].values.reshape(-1, 1)).todense(), columns= ["{}_{}".format(column_name, i) for i in range(real_data[column_name].nunique())], index= real_data.index)
+            real_data_oh = pd.concat((real_data_oh, temp))
+            temp = pd.DataFrame(ohe.transform(synthetic_data[column_name].values.reshape(-1, 1)).todense(), columns=["{}_{}".format(column_name, i) for i in range(real_data[column_name].nunique())], index=synthetic_data.index)
+            synthetic_data_oh = pd.concat((synthetic_data_oh, temp))
+        else:
+            real_data_oh = pd.concat((real_data_oh, real_data[[column_name]]))
+            synthetic_data_oh = pd.concat((synthetic_data_oh, synthetic_data[[column_name]]))
+    scaler = StandardScaler()
+    real_data_scaled = pd.DataFrame(scaler.fit_transform(real_data_oh), columns=real_data_oh.columns)
+    synthetic_data_scaled = pd.DataFrame(scaler.transform(synthetic_data_oh), columns=synthetic_data_oh.columns)
+    sample_size = 100
+    i = 0
+    min_distances = np.array([])
+    while i < synthetic_data_scaled.shape[0]:
+        # Calculate the Euclidean distances
+        synthetic_data_chunk = synthetic_data_scaled.iloc[i:i + sample_size]
+        distances = cdist(synthetic_data_chunk.values, real_data_scaled.values, metric='euclidean')
+        # Find the minimum distance for each synthetic record
+        min_distances = np.concatenate((min_distances, distances.min(axis=1)))
+        i += sample_size
+    mean_dcr = np.mean(min_distances)
+    median_dcr = np.median(min_distances)
+    median_dcr_5th = np.median(np.percentile(min_distances, 5))
+    privacy_dict = {"Privacy Mean": mean_dcr, "Privacy Median": median_dcr, "Privacy Median 5th": median_dcr_5th}
+
+    # Store the results per model
+    results_df = pd.concat((results_df, pd.DataFrame({**fidelity_dict, **synthesis_dict, **privacy_dict}, index=[model])))
+
+    results_df.drop_duplicates()
+    results_df.to_excel("./results/evaluation.xlsx", index=False)
+
+results_df.to_excel("./results/evaluation.xlsx", index=False)
---
+++ evaluation/RealBank/04_evaluation_EvaluateGraph.py
@@ -0,0 +1,119 @@
+import os.path
+import pickle
+
+from tqdm import tqdm
+
+import numpy as np
+import pandas as pd
+
+from scipy.spatial.distance import pdist, cdist
+
+from sklearn.cluster import KMeans, MiniBatchKMeans
+from sklearn.preprocessing import StandardScaler
+
+import networkx as nx
+
+from modified_sitepackages.netcomp import netsimile, deltacon0
+
+
+models = ['DOPPELGANGER', 'FINDIFF', 'TVAE', 'WGAN', 'CTGAN']
+keep_col = ['PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10']
+
+real_data = pd.read_csv("./working/transformed_df_graph.csv")
+#real_data = real_data.sample(100000)
+## get the unique nodes for real data
+real_target_nodes = real_data[["target_id_{}".format(i) for i in range(6)]]
+real_target_nodes.columns = ["id_{}".format(i) for i in range(6)]
+real_source_nodes = real_data[["source_id_{}".format(i) for i in range(6)]]
+real_source_nodes.columns = ["id_{}".format(i) for i in range(6)]
+real_nodes = pd.concat((real_target_nodes, real_source_nodes), axis= 0).drop_duplicates()
+num_real_nodes = real_nodes.shape[0]
+## calculate average distance between the nodes for real data
+scaler = StandardScaler()
+real_nodes_scaled = pd.DataFrame(scaler.fit_transform(real_nodes), columns=real_nodes.columns)
+real_nodes_avg_distance = []
+for i, chunk in tqdm(real_nodes_scaled.groupby(np.arange(len(real_nodes_scaled))//15000), desc= "Calculating Average Distance between Real Nodes"):
+    real_nodes_avg_distance.append(np.mean(cdist(real_nodes_scaled.drop(chunk.index).values, chunk.values, 'euclid')))
+real_nodes_avg_distance = np.mean(real_nodes_avg_distance)
+
+if not os.path.exists("./results"):
+    os.makedirs("./results")
+
+# transform data back into graph structure
+for model in models:
+    print("Currently Processing Model: {}".format(model))
+
+    with open("./model/{}.pkl".format(model), 'rb') as file:
+        synthesizer = pickle.load(file)
+
+    # sample in batches
+    synthetic_data = pd.DataFrame()
+    for i in tqdm(range(0, real_data.shape[0], 500000), desc= "Generate Synthetic Data"):
+        synthetic_data = pd.concat((synthetic_data, synthesizer.sample(num_rows=500000)))
+
+    # Restore Graph Structure
+    ## Use KNN to find transactions refering to the same node
+    ## optimize to keep the average distance between the nodes similar between real and synthetic data
+    ## get the unique nodes for synthetic data
+    synthetic_target_nodes = synthetic_data[["target_id_{}".format(i) for i in range(6)]]
+    synthetic_target_nodes.columns = ["id_{}".format(i) for i in range(6)]
+    synthetic_source_nodes = synthetic_data[["source_id_{}".format(i) for i in range(6)]]
+    synthetic_source_nodes.columns = ["id_{}".format(i) for i in range(6)]
+    synthetic_nodes = pd.concat((synthetic_target_nodes, synthetic_source_nodes), axis=0).drop_duplicates()
+
+    synth_nodes_avg_distances = {}
+    synthetic_nodes_scaled = pd.DataFrame(scaler.fit_transform(synthetic_nodes), columns=synthetic_nodes.columns)
+
+    for n_clusters in tqdm(range(int(num_real_nodes*0.25), int(num_real_nodes*1.5), 30000), desc="Search for optimal Number of Nodes"):
+        if not os.path.exists("./working/kmeans_{}/kmeans_{}_{}.pkl".format(model, model, n_clusters)):
+            kms = MiniBatchKMeans(n_clusters= n_clusters, init= "k-means++", n_init= "auto", batch_size= 8192)
+            kms.fit(synthetic_nodes_scaled)
+            if not os.path.exists("./working/kmeans_{}".format(model)):
+                os.makedirs("./working/kmeans_{}".format(model))
+            with open("./working/kmeans_{}/kmeans_{}_{}.pkl".format(model, model, n_clusters), 'wb') as f:
+                pickle.dump(kms, f)
+        else:
+            with open("./working/kmeans_{}/kmeans_{}_{}.pkl".format(model, model, n_clusters), 'rb') as f:
+                kms = pickle.load(f)
+
+        new_synthetic_nodes = pd.DataFrame(kms.cluster_centers_)
+
+        synthetic_nodes_avg_distance = []
+        for i, chunk in new_synthetic_nodes.groupby(np.arange(len(new_synthetic_nodes)) // 15000):
+            synthetic_nodes_avg_distance.append(np.mean(cdist(new_synthetic_nodes.drop(chunk.index).values, chunk.values, 'euclid')))
+        synthetic_nodes_avg_distance = np.mean(synthetic_nodes_avg_distance)
+
+        synth_nodes_avg_distances[n_clusters] = synthetic_nodes_avg_distance
+
+    n_cluster = min(synth_nodes_avg_distances, key=lambda x:abs(x- real_nodes_avg_distance))
+    with open("./working/kmeans_{}/kmeans_{}_{}.pkl".format(model, model, n_cluster), 'rb') as f:
+        kms = pickle.load(f)
+    synthetic_nodes["node_id"] = kms.predict(synthetic_nodes)
+
+    synthetic_data = synthetic_data.merge(synthetic_nodes, left_on= ["target_id_{}".format(i) for i in range(6)], right_on= ["id_{}".format(i) for i in range(6)], how="left", suffixes= ("", "_target"))
+    synthetic_data = synthetic_data.merge(synthetic_nodes, left_on=["source_id_{}".format(i) for i in range(6)], right_on=["id_{}".format(i) for i in range(6)], how="left", suffixes= ("", "_source"))
+
+    synthetic_data.to_csv("./synth/{}_synthetic_data_graph.csv".format(model), index=False)
+
+real_data = real_data.reset_index()[["source_id", "target_id", "index"]].groupby(["source_id", "target_id"]).count().reset_index().rename(columns= {"index": "weight"})
+real_graph = nx.from_pandas_edgelist(real_data, source= "source_id", target= "target_id", edge_attr="weight", create_using= nx.DiGraph)
+
+#Calculate the similarity in graph structure
+
+results_df = pd.DataFrame()
+for model in models:
+    synthetic_data = pd.read_csv("./synth/{}_synthetic_data_graph.csv".format(model))
+
+    # create networkx graphs based on the data
+    synthetic_data = synthetic_data.reset_index()[["node_id_source", "node_id_target", "index"]].groupby(["node_id_source", "node_id_target"]).count().reset_index().rename(columns= {"index": "weight"})
+    synthetic_graph = nx.from_pandas_edgelist(synthetic_data, source= "node_id_source", target= "node_id_target", edge_attr="weight", create_using= nx.DiGraph)
+
+    # score NetSimile
+    netsimile_distance = netsimile(nx.adjacency_matrix(real_graph), nx.adjacency_matrix(synthetic_graph))
+    # score deltacon0
+    deltacon0_distance = deltacon0(nx.adjacency_matrix(real_graph), nx.adjacency_matrix(synthetic_graph))
+
+    results_df = results_df.append({"Model": model, "NetSimile": netsimile_distance, "DeltaCon0": deltacon0_distance}, ignore_index=True)
+    results_df.to_excel("./results/evaluation_graph.xlsx", index=False)
+
+results_df.to_excel("./results/evaluation_graph.xlsx", index=False)
